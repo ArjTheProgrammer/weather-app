7c71065261d9a60fef09690e9160967ecfc477ea
Jose Neil Silagan <neilsilagan@gmail.com>
2024-09-10 16:47:12 +0800

    Make the app run

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import WeatherAPI from "./modules/weatherAPI";
+import "./styles.css";
+import WeatherApp from "./modules/weatherApp";
+import getLocation from "./modules/getLocation";
 
     const baseUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
     const apiKey = "5SSD5LWXSL6XMNLUZCTR5RCYA";
+
+    const weatherApp = new WeatherApp(baseUrl, apiKey);
+
+    getLocation(weatherApp);
+
+    weatherApp.displayWeatherApp("manila");
