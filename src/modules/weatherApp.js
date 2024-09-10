f596c6486c959430729d95b02fa9cbbc99c08c75
Jose Neil Silagan <neilsilagan@gmail.com>
2024-09-10 16:36:45 +0800

    Remove the input and created another file for it

--- src/modules/weatherApp.js
+++ src/modules/weatherApp.js
@@ -6,24 +6,8 @@ export default class WeatherApp {
         this.weatherAPI = new WeatherAPI(baseUrl, apiKey);
     }
 
-    async displayWeatherApp(){
-        const header = document.querySelector("header");
-        const main = document.querySelector("main");
-
-        const inputCon = document.createElement("div");
-        inputCon.className = "input-container";
-        header.append(inputCon);
-
-        const locationInput = document.createElement("input");
-        location.className = "location-input";
-        location.placeholder = "Input location";
-        inputCon.append(locationInput);
-
-        const inputBtn = document.createElement("button");
-        inputBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8" /><line x1="21" y1="21" x2="16.65" y2="16.65" /></svg>`;
-        inputCon.append(inputBtn);
-
-        const weather = await this.weatherAPI.getWeather(location.value == "" ? "manila" : location.value);
+    async displayWeatherApp(location){
+        const weather = await this.weatherAPI.getWeather(location);
 
         const weatherData = new WeatherData(
             weather.days[0].icon,
@@ -41,19 +25,6 @@ export default class WeatherApp {
             weather.days[0].dew,
             weather.days
         );
-
-        inputBtn.addEventListener("click", () => {
-            main.innerHTML = "";
-
-            weatherData.displayCurrentWeather();
-            weatherData.displayWeeklyWeather();
-        });
-
-        document.addEventListener("keydown", e => {
-            if (e.key == "Enter"){
-                inputBtn.click();
-            }
-        });
 
         weatherData.displayCurrentWeather();
         weatherData.displayWeeklyWeather();
